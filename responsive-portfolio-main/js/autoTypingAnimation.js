bbd36a4d7ac10bf724d451b5a44a203be4fb9efd
cyber awareness <160382360+cybertech64@users.noreply.github.com>
2024-04-06 08:40:00 -0700

    Add files via upload

---
+++ responsive-portfolio-main/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Suryansh Patel</span>  I do every work without wasting time, first I analyze your plan and then work on it, I am a freelancer, webdeveloper and problem solver."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
